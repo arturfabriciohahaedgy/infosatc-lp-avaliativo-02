0692d83b4141914eece611e60deea4d0eb912895
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-09-07 14:27:44 -0300

    feat: adiciona atividade-Ip-04

---
+++ exercicio04.py
@@ -0,0 +1,11 @@
+listaFilmes = ["Eraserhead", "Naked Lunch"]
+listaJogos = ["SMT III: Nocturne", "The Silver Case"]
+listaLivros = ["Hamlet", "Enquanto agonizo"]
+listaEsporte = ["Football", "Volley"]
+
+# exercicio A
+listaGeral = listaFilmes + listaJogos + listaLivros + listaEsporte
+print(listaGeral)
+
+#exercicio B
+print("Mostrar: ", listaGeral[4])
