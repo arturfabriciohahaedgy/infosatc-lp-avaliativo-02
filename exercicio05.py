4e9cf7485e4f7a73b30fc6c5038e241c3ba636a9
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-09-07 14:54:15 -0300

    feat: adiciona atividade-Ip-05

---
+++ exercicio05.py
@@ -0,0 +1,8 @@
+PCgamer = ["Processador Intel Core i5-9400F", "MASTERBOX Q500L", "GeForce RTX 2080 TI", "Memória XPG Spectrix", "Placa-Mãe Gigabyte GA-A320M-S2H"] 
+
+print("Os itens comprados serão: ", PCgamer)
+
+#exercicio A
+del PCgamer[1]
+del PCgamer[2]
+print("Os itens comprados serão: ", PCgamer)
