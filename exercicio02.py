743a167bfd522f13ea9b413a7ae5f49f457c87da
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-09-07 13:25:45 -0300

    feat: adiciona atividade-Ip-02

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+lista1 = [1, 2, 3, 4, 5]
+lista2 = []
+lista3 = lista1 + lista2
+
+print(lista3)
+
+lista2.append(lista1)
+print(lista2)
+
+#no enunciado "copiar uma lista em outra" ficou um pouco abstrato de entender, pelo menos para mim, então coloquei dois jeitos que eu acho que
+#eram o que a professora pediu com a atividade.
